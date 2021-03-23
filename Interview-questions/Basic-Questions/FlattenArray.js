415ae705c5c616d646e4ac0a90148d29a850c205
cr7yash <yashaqua@gmail.com>
2021-03-23 16:06:45 +0530

    Add Pseudocode

--- Interview-questions/Basic-Questions/FlattenArray.js
+++ Interview-questions/Basic-Questions/FlattenArray.js
@@ -1,3 +1,8 @@
+// Concept here is very simple, in our flattenArray function we will iterate through each item in an array and check if it is an array using native Array.isArray function.
+
+// If item is an array, we will iteratively call flattenArray function and concat it to to results
+// If item is not an array we will push item in the results array
+
 function FlattenArray(arr) {
 	let results = [];
 	for (let i = 0; i < arr.length; i++) {
