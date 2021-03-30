eccc8d7cb2d323b40f25cbe1a92e8bb6e7e03711
cr7yash <yashaqua@gmail.com>
2021-03-30 21:55:31 +0530

    Final solution

--- Javascript-DSA/Sorting-Algorithms/MergeSort.js
+++ Javascript-DSA/Sorting-Algorithms/MergeSort.js
@@ -22,19 +22,19 @@ function MergeArrays(arr1, arr2) {
 	let j = 0;
 
 	while (i < arr1.length && j < arr2.length) {
-		if (arr[j] > arr[i]) {
-			results.push(arr[i]);
+		if (arr2[j] > arr1[i]) {
+			results.push(arr1[i]);
 			i++;
 		} else {
-			results.push(arr[j]);
+			results.push(arr2[j]);
 			j++;
 		}
 		while (i < arr1.length) {
-			results.push(arr[i]);
+			results.push(arr1[i]);
 			i++;
 		}
 		while (j < arr2.length) {
-			results.push(arr[j]);
+			results.push(arr2[j]);
 			j++;
 		}
 	}
@@ -45,3 +45,15 @@ function MergeArrays(arr1, arr2) {
 // 1. Break up the array into halves until you have arrays that are empty or have one element.
 // 2. Once you have smaller sorted arrays, merge those arrays with other sorted arrays until you are back at the full length of the array.
 // 3. Once the array has been merged back together, return the merged(and sorted) array.
+
+function MergeSort(arr) {
+	if (arr.length <= 1) return arr;
+
+	let mid = Math.floor(arr.length / 2);
+	let left = MergeSort(arr.slice(0, mid));
+	let right = MergeSort(arr.slice(mid));
+
+	return MergeArrays(left, right);
+}
+
+console.log(MergeSort([8, 4, 34, 56, 23, 5, 10])); //=
